427e6abfab27db1de127add7c67b3c8ae1bed98e
svenigan01 <vsrikanth031020@gmail.com>
2026-09-22 08:48:55 -0700

    Add DAY02 runnable doubly linked list
    Implement a doubly linked list with append and print methods.

---
+++ DAY02/doubly_linked_list.py
@@ -0,0 +1,41 @@
+"""DAY02: doubly linked list with O(1) append."""
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def print_list(self):
+        current = self.head
+        while current is not None:
+            print(current.value)
+            current = current.next
+
+
+if __name__ == "__main__":
+    my_doubly_linked_list = DoublyLinkedList(7)
+    my_doubly_linked_list.append(8)
+
+    my_doubly_linked_list.print_list()
